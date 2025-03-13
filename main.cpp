b62c76fbb013737b4252260188af286865c353ba
Rahul Saravanan <128274814+BlastBringer@users.noreply.github.com>
2025-03-13 16:02:35 +0530

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,10 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins by PES1UG22AM129 Task5\n";
+   
+  return 0; 
+}
